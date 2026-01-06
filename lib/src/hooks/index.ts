14fe9875769cec4153158335d17bea61a45c3561
ghtjd1358 <hojjang18@naver.com>
2026-01-06 15:12:49 +0900

    lib 폴더 추가 및 경로 수정

---
+++ lib/src/hooks/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Hooks 모듈 - KOMCA 패턴
+ */
+
+// Auth Hooks
+export * from './use-auth';
+
+// Initialize Hook
+export * from './use-initialize';
+
+// Track History Hook
+export * from './use-track-history';
+
+// Navigate Hook
+export * from './use-navigate';
+
+// Global Loading Hook
+export * from './use-global-loading';
+
+// Modal Hooks
+export * from './use-modal';
+
+// Error Notification Hook
+export * from './use-error-notification';
---
+++ lib/src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @mfa/lib - MFA 공통 라이브러리
+ *
+ * KOMCA 패턴 기반 마이크로프론트엔드 공통 유틸리티
+ */
+
+// Types
+export * from './types';
+
+// Store Access
+export * from './store';
+
+// Utils
+export * from './utils';
+
+// Network (Axios Factory)
+export * from './network';
+
+// Hooks
+export * from './hooks';
